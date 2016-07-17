0ec300309293b7819b932a6e202b1096ad7b3145
Michael Herman <hermanmu@gmail.com>
2016-07-17 10:43:59 -0600

    minor changes

--- manage.py
+++ manage.py
@@ -32,7 +32,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
-    """Runs the unit tests without coverage."""
+    """Runs the unit tests without test coverage."""
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
