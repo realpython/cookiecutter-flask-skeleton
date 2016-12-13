9b9f8c038168919fc372832df5174519c9dc9224
Michael Herman <hermanmu@gmail.com>
2016-12-12 22:58:19 -0700

    updated dependencies

--- project/server/user/forms.py
+++ project/server/user/forms.py
@@ -1,20 +1,25 @@
 # project/server/user/forms.py
 
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email Address', [DataRequired(), Email()])
     password = PasswordField('Password', [DataRequired()])
 
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
     email = StringField(
         'Email Address',
-        validators=[DataRequired(), Email(message=None), Length(min=6, max=40)])
+        validators=[
+            DataRequired(),
+            Email(message=None),
+            Length(min=6, max=40)
+        ]
+    )
     password = PasswordField(
         'Password',
         validators=[DataRequired(), Length(min=6, max=25)]
